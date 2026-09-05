39cf0277c492f44b9af2f99beded4f8a8e58ba85
00adrn <adrianvez11@gmail.com>
2026-09-05 02:32:30 -0400

    Added binary to .gitignore. Added flags to exec.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"flag"
 
 	"gvpn/client"
 	"gvpn/server"
@@ -10,11 +11,26 @@ import (
 func main() {
 	fmt.Println("Starting vpn service...")
 
-	fmt.Println("Starting server...")
-	go server.Start()
+	mode := flag.String("mode", "server", "Mode to run the program in: \"server\" or \"client\"")
+	flag.Parse()
 
-	fmt.Println("Starting client...")
-	go client.Start()
+	switch * mode {
+		case "server": {
+			fmt.Println("Starting server mode...")
+			go server.Start()
+		}
+		case "client": {
+			fmt.Println("Starting client mode...")
+			go client.Start()
+		}
+		case "test" : {
+			fmt.Println("Starting same machine testing mode...")
+			go client.Start()
+			go server.Start()
+		}
+		default:
+			fmt.Println("Invalid mode. Use 'server' or 'client'.")
+	}
 
 	for {}
 }
